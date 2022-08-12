f26ba99178e8b46551fccbcb4e6cfdf4a3d00f65
nanogada <jshin2992@gmail.com>
2022-08-13 04:18:37 +0900

    login

--- public/js/home/login.js
+++ public/js/home/login.js
@@ -1,2 +1,11 @@
-console.log("hello");
-console.log('bye')
+const id = document.querySelector("#id"),
+    psword = document.querySelector("#pwd"),
+    loginbtn = document.querySelector("#login_button");
+console.log(id);
+loginbtn.addEventListener("click", () => {
+    const req={
+        id: id.value,
+        psword: psword.value,
+    };
+    console.log(req);
+});
